bbdb5e2a8bbf797a41c0fa9eddca63e55c4bf414
Ozge <erolozge01@gmail.com>
2018-11-07 16:33:39 -0600

    Remove duplicated directory

--- tipcalculator.py
+++
@@ -1,9 +0,0 @@
-#The following example is the answer of tip calculator project in Codecademy.
-meal = 44.50 #the price of meal.
-tax = 0.0675 
-tip = 0.15 # at least %15
-meal = meal + (meal * tax)
-total = meal + (meal * tip)
-
-print("%.2f" % total)
-
